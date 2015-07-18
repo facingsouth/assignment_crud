10bb005ce9a6befcc3e3a03ff76b8ef2c53fb0f5
facingsouth <xin.d.liu@gmail.com>
2015-07-18 10:56:39 -0700

    add create index show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,23 @@ class ItemsController < ApplicationController
   end
 
   def index
-
+    @items = Item.all
   end
+
+  def create
+    @item = Item.new(:title => params[:item][:title], :des => params[:item][:des], :price => params[:item][:price].to_i)
+    if @item.save
+      flash[:notice] = "Item saved"
+      redirect_to item_path(@item.id)
+    else
+      flash.now[:notice] = "Invalid input"
+      render :new_item_path
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :items, only: [:new]
+  resources :items, only: [:new, :index, :create, :show]
 
   
 
