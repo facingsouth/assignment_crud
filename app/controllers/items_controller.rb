14dd7a5ea47007346c8324cace2fef28a98ba3dc
mikebutts <mgbutts@gmail.com>
2015-07-18 14:31:21 -0400

    completed ugly app

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,29 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update 
+     @item = Item.find(params[:id])
+    if @item.update(:title => params[:item][:title], :des => params[:item][:des], :price => params[:item][:price].to_i)
+      flash[:notice] = "Item saved"
+      redirect_to item_path(@item.id)
+    else
+      flash.now[:notice] = "Invalid input"
+      render :new_item_path
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path, notice: 'Items was successfully deleted.'
+  end
+
 
 
 end
